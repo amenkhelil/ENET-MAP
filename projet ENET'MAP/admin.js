062c705d0bfac57d0c36b114e63b7824f8cda786
Amen <khalilamen02@gmail.com>
2025-02-24 19:43:16 +0100

    Delete projet ENET'MAP/admin.js

--- projet ENET'MAP/admin.js
+++
@@ -1,9 +0,0 @@
-const mobileMenuButton = document.getElementById("mobileMenu");
-        const navLinks = document.querySelector(".nav-links");
-
-
-        mobileMenuButton.addEventListener("click", () => {
-            navLinks.classList.toggle("active");
-            mobileMenuButton.classList.toggle("menu-open");
-        });
-  
